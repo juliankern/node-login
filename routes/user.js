9cf9f666a4dca1b5931bd5fcd34ab6acdf5eed9e
Julian Kern <mail@juliankern.com>
2017-07-13 11:15:39 +0200

    Multiple changes

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 var user = require('../controllers/user.js');
+var security = require('../controllers/security');
 
 module.exports = (app) => {
-    app.get('/user', async (req, res) => {
+    app.get('/user', security.isLoggedin, async (req, res) => {
         res.render('userlist', { users: await user.get() });
     });
 }
